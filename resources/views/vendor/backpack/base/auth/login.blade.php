490d03a3a8c0ec979c2dd54edd33313ea2e0c56d
Slman Ali <slman@wizme.com>
2020-09-13 16:43:41 +0300

    remove forget password

--- resources/views/vendor/backpack/base/auth/login.blade.php
+++ resources/views/vendor/backpack/base/auth/login.blade.php
@@ -57,9 +57,6 @@
                     </form>
                 </div>
             </div>
-            @if (backpack_users_have_email())
-                <div class="text-center"><a href="{{ route('backpack.auth.password.reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a></div>
-            @endif
             @if (config('backpack.base.registration_open'))
                 <div class="text-center"><a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></div>
             @endif
